802d187c562306906ad13610c4e9764e1a56043d
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2025-05-04 21:48:39 +0200

    Run rustfmt
    Signed-off-by: C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>

--- testing/tests/cow_str_implements_fast_writable.rs
+++ testing/tests/cow_str_implements_fast_writable.rs
@@ -1,16 +1,17 @@
 use std::borrow::Cow;
+
 use askama::{FastWritable, Template};
 
-//This tests, that Cow<'_, str> implements FastWritable.
-//If it didn't, compiling the tests would fail.
+// This tests, that Cow<'_, str> implements FastWritable.
+// If it didn't, compiling the tests would fail.
 //
-//Since this test fails at compile-time, there isn't actually any need to run it.
+// Since this test fails at compile-time, there isn't actually any need to run it.
 #[allow(dead_code)]
-fn cow_str_fast_writable(){
+fn cow_str_fast_writable() {
     #[derive(Template)]
-    #[template(source = "{{content|safe}}", ext="txt")]
-    //Todo: need to explicitly mark content as |safe?
-    //error[E0599]: the method `askama_auto_escape` exists for reference `&&AutoEscaper<'_, T, Text>`, but its trait bounds were not satisfied
+    #[template(source = "{{content|safe}}", ext = "txt")]
+    // Todo: need to explicitly mark content as |safe?
+    // error[E0599]: the method `askama_auto_escape` exists for reference `&&AutoEscaper<'_, T, Text>`, but its trait bounds were not satisfied
     //  --> testing/tests/cow_str_implements_fast_writable.rs:9:14
     //   |
     // 9 |     #[derive(Template)]
@@ -24,8 +25,9 @@ fn cow_str_fast_writable(){
         pub content: T,
     }
 
-    Test{
-        content: Cow::Borrowed("")
-    }.render()
-        .unwrap();
+    Test {
+        content: Cow::Borrowed(""),
+    }
+    .render()
+    .unwrap();
 }
